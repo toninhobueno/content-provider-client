ab632b73bd49b3f548b60f292e81b99a1843e880
tonin <antonio.leite00@hotmail.com>
2021-08-15 20:39:44 -0300

    criado recylcer view e adapter

--- app/src/main/java/github/toninhobueno/contentproviderclient/MainActivity.kt
+++ app/src/main/java/github/toninhobueno/contentproviderclient/MainActivity.kt
@@ -2,10 +2,25 @@ package github.toninhobueno.contentproviderclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var notesRecycler : RecyclerView
+    lateinit var noteRefreshButton: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    noteRefreshButton = findViewById(R.id.client_buttom_refresh)
+    notesRecycler = findViewById(R.id.client_list)
+
+    noteRefreshButton.setOnClickListener{
+
+    }
     }
 }
