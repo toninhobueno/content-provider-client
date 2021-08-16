bbcf05e61745ef1a4b1f30ebaae85f81393fb4d8
tonin <antonio.leite00@hotmail.com>
2021-08-15 21:00:05 -0300

    chamando dados do content provider e atualizando-os

--- app/src/main/java/github/toninhobueno/contentproviderclient/MainActivity.kt
+++ app/src/main/java/github/toninhobueno/contentproviderclient/MainActivity.kt
@@ -1,10 +1,14 @@
 package github.toninhobueno.contentproviderclient
 
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,8 +23,27 @@ class MainActivity : AppCompatActivity() {
     noteRefreshButton = findViewById(R.id.client_buttom_refresh)
     notesRecycler = findViewById(R.id.client_list)
 
-    noteRefreshButton.setOnClickListener{
+    getContentProvider()
 
+    noteRefreshButton.setOnClickListener{
+        getContentProvider()
+    }
+
+
+    }
+
+    private fun getContentProvider() {
+    try {
+        val url = "content://github.toninhobueno.contentprovider.provider/notes"
+        val data = Uri.parse(url)
+        val cursor: Cursor? = contentResolver.query(data, null , null , null , "title")
+        notesRecycler.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = ClientAdapter(cursor as Cursor)
+        }
+
+    }catch (ex : Exception){
+        ex.printStackTrace()
     }
     }
 }
